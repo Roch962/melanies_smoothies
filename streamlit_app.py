221c85d9d9da41b235673e21dffc6acd7b9035ea
Roch962 <rochellehillsx@icloud.com>
2024-12-18 17:18:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ', '.join(ingredients_list)  # Concatenate selected ingredients into a string
-        st.subheader(fruit_chosen + 'Nutrition Information')
+    st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # Create the SQL insert statement
